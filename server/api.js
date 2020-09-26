ad3d45903595b76f9961025ccd100ace0d61cc49
Daniel Jonsson <daniel@jonsson.xyz>
2020-09-26 12:24:18 +0200

    Solve second part with ideas APIs

--- server/api.js
+++ server/api.js
@@ -36,4 +36,37 @@ apiRouter.delete('/minions/:minionId', (req, res, next) => {
   res.status(204).send();
 });
 
+apiRouter.param('ideaId', (req, res, next, id) => {
+  const idea = db.getFromDatabaseById('ideas', id);
+  if (!idea) {
+    res.status(404).send();
+  } else {
+    req.idea = idea;
+    next();
+  }
+});
+
+apiRouter.get('/ideas', (req, res, next) => {
+  res.send(db.getAllFromDatabase('ideas'));
+});
+
+apiRouter.post('/ideas', (req, res, next) => {
+  let idea = db.addToDatabase('ideas', req.body);
+  res.status(201).send(idea);
+});
+
+apiRouter.get('/ideas/:ideaId', (req, res, next) => {
+  res.send(req.idea);
+});
+
+apiRouter.put('/ideas/:ideaId', (req, res, next) => {
+  let idea = db.updateInstanceInDatabase('ideas', req.body);
+  res.status(201).send(idea);
+});
+
+apiRouter.delete('/ideas/:ideaId', (req, res, next) => {
+  db.deleteFromDatabasebyId('ideas', req.idea.id);
+  res.status(204).send();
+});
+
 module.exports = apiRouter;
