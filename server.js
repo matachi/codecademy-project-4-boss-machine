51ac619e9ed64fb3cd931fa6b96fd53d02c4ec2f
Daniel Jonsson <daniel@jonsson.xyz>
2020-09-26 12:21:27 +0200

    Solve first part with minions APIs

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 module.exports = app;
@@ -9,17 +11,17 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+app.use(cors());
 
 // Add middware for parsing request bodies here:
-
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
-
+app.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
-
+  app.listen(PORT);
 }
--- server/api.js
+++ server/api.js
@@ -1,6 +1,39 @@
 const express = require('express');
+const db = require('./db');
+
 const apiRouter = express.Router();
 
+apiRouter.param('minionId', (req, res, next, id) => {
+  const minion = db.getFromDatabaseById('minions', id);
+  if (!minion) {
+    res.status(404).send();
+  } else {
+    req.minion = minion;
+    next();
+  }
+});
 
+apiRouter.get('/minions', (req, res, next) => {
+  res.send(db.getAllFromDatabase('minions'));
+});
+
+apiRouter.post('/minions', (req, res, next) => {
+  let minion = db.addToDatabase('minions', req.body);
+  res.status(201).send(minion);
+});
+
+apiRouter.get('/minions/:minionId', (req, res, next) => {
+  res.send(req.minion);
+});
+
+apiRouter.put('/minions/:minionId', (req, res, next) => {
+  let minion = db.updateInstanceInDatabase('minions', req.body);
+  res.status(201).send(minion);
+});
+
+apiRouter.delete('/minions/:minionId', (req, res, next) => {
+  db.deleteFromDatabasebyId('minions', req.minion.id);
+  res.status(204).send();
+});
 
 module.exports = apiRouter;
